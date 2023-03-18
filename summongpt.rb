e38c6a52db4b5952bebd2230f7ab8bbeaf5f73bb
Fayez Nazzal <fayeznazzal@Fayezs-MacBook-Air.local>
2023-03-18 19:29:11 +0200

    Add head line

--- summongpt.rb
+++ summongpt.rb
@@ -1,6 +1,7 @@
 class Summongpt < Formula
   desc "Summon the power of ChatGPT using a keyboard shortcut."
   homepage "https://github.com/fayez-nazzal/summongpt"
+  head "https://github.com/fayez-nazzal/summongpt.git"
 
   depends_on "rust"
   depends_on "node"
