f2f9086e6a9d7c46982f96a545ca72afa206d262
Fayez Nazzal <fayeznazzal@Fayezs-MacBook-Air.local>
2023-03-18 19:26:01 +0200

    Remove unused lines

--- summongpt.rb
+++ summongpt.rb
@@ -1,6 +1,6 @@
 class Summongpt < Formula
-  desc "A description of your Tauri application"
-  homepage "https://github.com/your-username/summongpt"
+  desc "Summon the power of ChatGPT using a keyboard shortcut."
+  homepage "https://github.com/fayez-nazzal/summongpt"
 
   depends_on "rust"
   depends_on "node"
@@ -11,10 +11,6 @@ class Summongpt < Formula
     system "npm", "run", "tauri", "build"
     prefix.install Dir["src-tauri/target/release/bundle/macos/summongpt"]
     bin.install_symlink prefix/"#{prefix.basename}.app/Contents/MacOS/summongpt"
-  end
-
-  test do
-    # Add a simple test if you like
   end
 end
 
