f74487badb663c76319c02d2b4829ed5416659e8
Fayez Nazzal <fayeznazzal@Fayezs-MacBook-Air.local>
2023-03-18 19:14:52 +0200

    Attempt to fix build errors

--- summongpt.rb
+++ summongpt.rb
@@ -1,8 +1,6 @@
 class Summongpt < Formula
   desc "A description of your Tauri application"
   homepage "https://github.com/your-username/summongpt"
-  url "https://github.com/your-username/summongpt/releases/download/v1.0.0/summongpt-v1.0.0.tar.gz"
-  sha256 "1e57a452a094728c291bc42bf2bc7eb8d9fd8844d1369da2bf728588b46c4e75"
 
   depends_on "rust"
   depends_on "node"
@@ -11,7 +9,8 @@ class Summongpt < Formula
     system "npm", "install"
     system "npm", "run", "build"
     system "npm", "run", "tauri", "build"
-    bin.install "src-tauri/target/release/summongpt"
+    prefix.install Dir["src-tauri/target/release/bundle/macos/summongpt"]
+    bin.install_symlink prefix/"#{prefix.basename}.app/Contents/MacOS/summongpt"
   end
 
   test do
